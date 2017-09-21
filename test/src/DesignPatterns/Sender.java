380baac99bcee604c02bb170cdb1282db6961bf4
yin <1320207218@qq.com>
2017-09-21 13:46:54 +0800

    请求

---
+++ test/src/DesignPatterns/Sender.java
@@ -0,0 +1,5 @@
+package DesignPatterns;
+
+public interface Sender {  
+    public void Send();  
+}
---
+++ test/src/code/codedemo1.java
@@ -0,0 +1,21 @@
+package code;
+
+public class codedemo1 {
+
+	public static void main(String[] args) {
+		int i=5;
+		int j=10;
+		
+		System.out.println(~10);
+		
+		System.out.println(i+~10);
+		
+		byte a=127;
+		System.out.println(++a);
+		System.out.println(a);
+		
+		System.out.println(a+200);
+		
+	}
+	
+}
